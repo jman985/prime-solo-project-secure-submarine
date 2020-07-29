89af1d5daa373981cc969507ca710a099d7c3d1e
Josh Maus <jman985@gmail.com>
2020-07-29 15:20:54 -0500

    added base goals

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
+const userStrategy = require('../strategies/user.strategy');
 
-router.get('/', (req, res) => {
+
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    pool.query('SELECT * FROM "secret" WHERE secrecy_level<=$1;',[req.user.clearance_level])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
 });
+
+
+// router.get('/', rejectUnauthenticated, (req, res) => {
+//     // Send back user object from the session (previously queried from the database)
+//     res.send(req.user);
+//   });
+
 
 module.exports = router;
